66312d027ff300e6388b793418626ed3e1ca5b31
Devin Carlen <devin.carlen@gmail.com>
2019-03-09 04:11:11 +0000

    Clean up example call

--- seattle.sh
+++ seattle.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 export http_proxy=proxy.orcltest1.com:80
-
-curl -X POST -H "Content-Type: application/json" -d '{"city": "Seattle"}' http://fn.lb.orcltest1.com:90/invoke/01D5A78MS31BT0A780A0000002
-
+curl -X POST -H "Content-Type: application/json" -d '{"city": "Seattle"}' `fn inspect fn --endpoint demo fn-weather`
